875e424952336be5a9aedc49e6c2faccc8e51569
naylajuarex <naylajuarezet12@gmail.com>
2022-08-29 10:22:37 -0300

    ..

--- BD/02-trigger.sql
+++ BD/02-trigger.sql
@@ -1,4 +1,4 @@
--- Realizar un trigger para que antes de ingresar una fila en Transferencia, verifique que el usuario posea al futbolista en cuestión. En caso de no poseerlo, no se debe permitir la inserción de la fila y se debe mostrar la leyenda “El usuario nombreUsuario no posee a al futbolista NombreApellido"
+/*Realizar un trigger para que antes de ingresar una fila en Transferencia, verifique que el usuario posea al futbolista en cuestión. En caso de no poseerlo, no se debe permitir la inserción de la fila y se debe mostrar la leyenda “El usuario nombreUsuario no posee a al futbolista NombreApellido"*/
 
 DELIMITER $$
 DROP TRIGGER IF EXISTS befinsTrasferencia $$
@@ -9,7 +9,7 @@ DECLARE Futbolistaj SMALLINT;
 SELECT  INTO Futbolistaj
 
 
--- Realizar un trigger para que antes de efectuar una compra, se verifique que el usuario comprador, posea al menos la cantidad suficiente de monedas para la compra; en caso de no poseer las monedas, se debe cancelar la operación y mostrar la leyenda “Monedas insuficientes”. En caso de que el comprador ya posea al futbolista en cuestión, tampoco se debe permitir la operación y se debe mostrar la leyenda “Jugador en posesión”.
+/* Realizar un trigger para que antes de efectuar una compra, se verifique que el usuario comprador, posea al menos la cantidad suficiente de monedas para la compra; en caso de no poseer las monedas, se debe cancelar la operación y mostrar la leyenda “Monedas insuficientes”. En caso de que el comprador ya posea al futbolista en cuestión, tampoco se debe permitir la operación y se debe mostrar la leyenda “Jugador en posesión”.*/
 
 DELIMITER $$
 DROP PROCEDURE IF EXISTS befinsTransferencia $$
