78e6611015a393d228957420f171e5fed74e849a
naylajuarex <naylajuarezet12@gmail.com>
2022-09-08 09:16:57 -0300

    .

--- BD/02-trigger.sql
+++ BD/02-trigger.sql
@@ -1,4 +1,6 @@
-/*Realizar un trigger para que antes de ingresar una fila en Transferencia, verifique que el usuario posea al futbolista en cuestión. En caso de no poseerlo, no se debe permitir la inserción de la fila y se debe mostrar la leyenda “El usuario nombreUsuario no posee a al futbolista NombreApellido"*/
+/*Realizar un trigger para que antes de ingresar una fila en Transferencia, 
+verifique que el usuario posea al futbolista en cuestión. En caso de no poseerlo, 
+no se debe permitir la inserción de la fila y se debe mostrar la leyenda “El usuario nombreUsuario no posee a al futbolista NombreApellido"*/
 
 DELIMITER $$                                 
 DROP TRIGGER IF EXISTS befinsTrasferencia $$                        
@@ -6,13 +8,19 @@ CREATE TRIGGER befinsTransferencia BEFORE INSERT ON Transferencia
 FOR EACH ROW
 BEGIN                        
 DECLARE FutbolistaJ SMALLINT;                                     
-SELECT  INTO FutbolistaJ
-FROM Posesion
+SELECT idFutbolista INTO FutbolistaJ
+FROM Transferencia
+JOIN Jugador USING (idJugador)
+WHERE idJugador = NEW.idVendedor;
+IF (EXISTS  (SELECT *
+                FROM    Transferencia
+                WHERE   idFutbolista != FutbolistaJ)) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'El usuario no posee al futbolista';
+END IF;
+END $$
 
-/* Realizar un trigger para que antes de efectuar una compra, se verifique que el usuario comprador, posea al menos la cantidad suficiente de monedas para la compra; en caso de no poseer las monedas, se debe cancelar la operación y mostrar la leyenda “Monedas insuficientes”. En caso de que el comprador ya posea al futbolista en cuestión, tampoco se debe permitir la operación y se debe mostrar la leyenda “Jugador en posesión”.*/
-
-DELIMITER $$
-DROP PROCEDURE IF EXISTS befinsTransferencia $$
-CREATE TRIGGER befinsTransferencia BEFORE INSERT ON Tranferencia
-FOR EACH ROW 
-
+/* Realizar un trigger para que antes de efectuar una compra, se verifique que el usuario comprador, 
+posea al menos la cantidad suficiente de monedas para la compra; en caso de no poseer las monedas, 
+se debe cancelar la operación y mostrar la leyenda “Monedas insuficientes”. En caso de que el comprador ya posea al futbolista en cuestión, 
+tampoco se debe permitir la operación y se debe mostrar la leyenda “Jugador en posesión”.*/
