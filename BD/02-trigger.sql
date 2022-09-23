5da10bd59d5ed3d24c5bb9cb688a786c697ef38c
naylajuarex <naylajuarezet12@gmail.com>
2022-09-23 09:16:58 -0300

    Update 02-trigger.sql

--- BD/02-trigger.sql
+++ BD/02-trigger.sql
@@ -2,28 +2,22 @@
 verifique que el usuario posea al futbolista en cuestión. En caso de no poseerlo, 
 no se debe permitir la inserción de la fila y se debe mostrar la leyenda “El usuario nombreUsuario no posee a al futbolista NombreApellido"*/
 
+/* Realizar un trigger para que antes de efectuar una compra, se verifique que el usuario comprador, 
+posea al menos la cantidad suficiente de monedas para la compra; en caso de no poseer las monedas, 
+se debe cancelar la operación y mostrar la leyenda “Monedas insuficientes”.*/
+
 DELIMITER $$                                 
 DROP TRIGGER IF EXISTS NoPosee $$                        
 CREATE TRIGGER NoPosee BEFORE INSERT ON Transferencia   
 FOR EACH ROW
 BEGIN                        
-IF (EXISTS  (SELECT *
+IF ( NOT EXISTS  (SELECT *
                 FROM    Posesion
-                WHERE   idFutbolista != FutbolistaJ)) THEN
+                WHERE   idFutbolista = FutbolistaJ
+                AND idJugador = NEW.id.Vendedor)) THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'El usuario no posee al futbolista';
 END IF;
-END $$
-
-/* Realizar un trigger para que antes de efectuar una compra, se verifique que el usuario comprador, 
-posea al menos la cantidad suficiente de monedas para la compra; en caso de no poseer las monedas, 
-se debe cancelar la operación y mostrar la leyenda “Monedas insuficientes”.*/
-
-DELIMITER $$
-DROP TRIGGER IF EXISTS BefInsTransferencia $$
-CREATE TRIGGER BefInsTransferencia BEFORE INSERT ON Transferencia
-FOR EACH ROW
-BEGIN 
 IF (EXISTS  (SELECT *
                 FROM    Jugador
                 WHERE   NEW.precio > moneda
@@ -33,12 +27,13 @@ IF (EXISTS  (SELECT *
 END IF;
 END $$
 
+
 /*En caso de que el comprador ya posea al futbolista en cuestión, 
 tampoco se debe permitir la operación y se debe mostrar la leyenda “Jugador en posesión”.*/
 
 DELIMITER $$
 DROP TRIGGER IF EXISTS TienePosesion $$
-CREATE TRIGGER BefInsTienePosesion BEFORE INSERT ON Transferencia
+CREATE TRIGGER BefInsTienePosesion BEFORE UPDATE ON Transferencia
 FOR EACH ROW
 BEGIN 
         IF (EXISTS  (SELECT *
