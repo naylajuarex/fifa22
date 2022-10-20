c4b2b446fef32533dcd7e1f96b31801c503db487
naylajuarex <naylajuarezet12@gmail.com>
2022-10-20 11:58:35 -0300

    :;.

---
+++ src/Fifa.AdoMysql/Class1.cs
@@ -0,0 +1,21 @@
+﻿using et12.edu.ar.AGBD.Mapeadores;
+using et12.edu.ar.AGBD.Ado;
+using System;
+using System.Data;
+using System.Collections.Generic;
+
+namespace Fifa.AdoMysql;
+public class MapFutbolista : Mapeador<Futbolista>
+{
+    public MapFutbolista(AdoAGBD ado) : base(ado)
+    {
+        tabla = "Futbolista";
+    }
+    public override Futbolista ObjetoDesdeFila(DataRow fila)
+            => new Futbolista()
+            {
+                idFutbolista = Convert.Toushort(fila["idFutbolista"]),
+                nombre = fila["Futbolista"].ToString()
+            };
+
+}
